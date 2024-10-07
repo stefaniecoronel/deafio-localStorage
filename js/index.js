ac2199ca7e79e11feaade2c1469556cc2076b2bc
stefaniecoronel <stefaniecoronel15@gmail.com>
2024-10-07 09:24:55 -0300

    Se agrega placeholder y opción de vaciar campo input

--- js/index.js
+++ js/index.js
@@ -2,4 +2,5 @@
 document.getElementById("buttonText").addEventListener('click', function(){
     let dato = document.getElementById("inputText").value
     localStorage.setItem("dato ingresado", dato)
+    document.getElementById("inputText").value =''
 })
